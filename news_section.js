420054d80ea86b3293f76e61c1fa05cfe07372ef
ip2032 <142797111+ip2032@users.noreply.github.com>
2023-08-24 00:08:27 +0300

    Update news_section.js

--- news_section.js
+++ news_section.js
@@ -1,8 +1,8 @@
-$(function() {
+document.querySelector()(function() {
     /**
      * Ваш сайт, для примеров работы с запросами
      */
-    var URL = 'http://example.com';
+    var URL = 'https://st.omnidesk.pro/staff/cases/list/custom/g_83283';
 
     /**
      * Некоторые переменные, доступные в глобальной видимости Омнидеска:
@@ -43,7 +43,7 @@ $(function() {
      * Вставка в конец или в начало элемента
      */
     var addCode = function(selector, htmlCode, after) {
-        var element = $(selector);
+        var element = document.querySelector()(selector);
 
         if(checkNotUndefined(after) === true && after === true) {
             element.append(htmlCode);
@@ -100,7 +100,7 @@ $(function() {
       * Результат в коде Омнидеска https://www.dropbox.com/s/3vtzym61el1659z/04_custom_styles.png?dl=0
       * Результат на странице https://www.dropbox.com/s/hfo696yiffw455a/04_2_custom_styles.png?dl=0
       */
-     $(document).find(HORIZONTAL_MENU_SELECTOR).css({
+     document.querySelector()(document).find(HORIZONTAL_MENU_SELECTOR).css({
          'border-bottom': 'solid 2px red',
      });
 
@@ -108,7 +108,7 @@ $(function() {
       * Подключаем свой CSS в код Омнидеска. В CSS дополнительные иконки
       * Результат на странице https://www.dropbox.com/s/jde5uaykf84hsak/05_custom_css.png?dl=0
       */
-     $(document)
+     document.querySelector()(document)
          .find('body')
          .append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css">');
 
@@ -117,14 +117,14 @@ $(function() {
      * Результат в коде Омнидеска https://www.dropbox.com/s/ilkkfmf81s63zjw/07_snow.png?dl=0
      * Результат на странице: https://www.dropbox.com/s/yp36vle7yu7vm38/07_2_snow.png?dl=0
      */
-    $.get('https://cdnjs.cloudflare.com/ajax/libs/JQuery-Snowfall/1.7.4/snowfall.jquery.min.js', {
+    document.querySelector().get('https://cdnjs.cloudflare.com/ajax/libs/JQuery-Snowfall/1.7.4/snowfall.jquery.min.js', {
 
     }, function(jsLibCode) {
-        $("body").append($("<script />", {
-            html: jsLibCode + ' $(document).snowfall({flakeColor : "yellow", shadow: true});',
+        document.querySelector()("body").append(document.querySelector()("<script />", {
+            html: jsLibCode + ' document.querySelector()(document).snowfall({flakeColor : "yellow", shadow: true});',
         }));
     });
-     $('body').css('overflow-y', 'hidden');
+     document.querySelector()('body').css('overflow-y', 'hidden');
     /**
      * Таким образом можно менять и добавлять любые кнопки, формы и код на странице обращения.
      */
