4e72f10a7a4a950a02dadd2d4f0f2c6e0d12f831
ip2032 <142797111+ip2032@users.noreply.github.com>
2023-08-24 10:29:27 +0300

    Update news_section.js

--- news_section.js
+++ news_section.js
@@ -1,3 +1,58 @@
+$(function() {
+    /**
+     * Ваш сайт, для примеров работы с запросами
+     */
+    var URL = 'http://example.com';
+
+    /**
+     * Некоторые переменные, доступные в глобальной видимости Омнидеска:
+     * CurrentCaseId
+     * CurrentUserId
+     * CurrentStaffId
+     * CurrentClientId
+     *
+     * Данные в этих переменных уже можно использовать чтобы получить более развернутый результат по API Омнидеска
+     * https://omnidesk.ru/api/introduction/intro
+     */
+
+    var STAFF_ID = CurrentStaffId;
+
+    /**
+     * Некоторые селекторы для примеров
+     */
+
+    var HORIZONTAL_MENU_SELECTOR = '.header-container';
+    var HORIZONTAL_MENU_BUTTONS_SELECTOR = '.global-actions > .global-actions-list:last-child';
+    var HORIZONTAL_MENU_ELEMENTS_SELECTOR = '.primary-nav';
+
+    var TOP_PANEL_SELECTOR = '#alpha1_panel';
+    var RECORD_SELECTOR = '.req-tr.req-data-row';
+    var RECORD_INFO_SELECTOR = '.req-td.req-inf';
+
+    /** HELPERS */
+
+    /**
+     * Проверяем на undefined
+     */
+    var checkNotUndefined = function(data) {
+        return (typeof data === 'undefined') ? false : true;
+    }
+
+
+    /**
+     * Вставка в конец или в начало элемента
+     */
+    var addCode = function(selector, htmlCode, after) {
+        var element = $(selector);
+
+        if(checkNotUndefined(after) === true && after === true) {
+            element.append(htmlCode);
+        } else {
+            element.prepend(htmlCode);
+        }
+    };
+
 document.querySelector()(document).find(HORIZONTAL_MENU_SELECTOR).css({
          'border-bottom': 'solid 2px red',
      });
+    }
